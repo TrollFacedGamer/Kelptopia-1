0f81e09f7b40af9aea5842157eb341d6fcc38576
Benny <158503969+TrollFacedGamer@users.noreply.github.com>
2024-07-26 01:01:22 -0400

    functioning number inputs and buttons on char creation

--- script.js
+++ script.js
@@ -6,71 +6,170 @@ const interface = document.getElementById("interface");
     const choices = document.getElementById("choices");
 const credits = document.getElementById("credits")
 //ElementStat
-const currentVitalityStat = document.getElementById("currentVitalityStat")
-const currentEnduranceStat = document.getElementById("currentEnduranceStat")
-const currentStrengthStat = document.getElementById("currentStrengthStat")
-const currentAgilityStat = document.getElementById("currentAgilityStat")
-const currentIntelligenceStat = document.getElementById("currentIntelligenceStat")
-const currentWisdomStat = document.getElementById("currentWisdomStat")
-const currentPerceptionStat = document.getElementById("currentPerceptionStat")
-const currentCharismaStat = document.getElementById("currentCharismaStat")
+const currentVitalityStat = document.getElementById("currentVitalityStat");
+const currentEnduranceStat = document.getElementById("currentEnduranceStat");
+const currentStrengthStat = document.getElementById("currentStrengthStat");
+const currentAgilityStat = document.getElementById("currentAgilityStat");
+const currentIntelligenceStat = document.getElementById("currentIntelligenceStat");
+const currentWisdomStat = document.getElementById("currentWisdomStat");
+const currentPerceptionStat = document.getElementById("currentPerceptionStat");
+const currentCharismaStat = document.getElementById("currentCharismaStat");
 
-const maxVitalityStat = document.getElementById("maxVitalityStat")
-const maxEnduranceStat = document.getElementById("maxEnduranceStat")
-const maxStrengthStat = document.getElementById("maxStrengthStat")
-const maxAgilityStat = document.getElementById("maxAgilityStat")
-const maxIntelligenceStat = document.getElementById("maxIntelligenceStat")
-const maxWisdomStat = document.getElementById("maxWisdomStat")
-const maxPerceptionStat = document.getElementById("maxPerceptionStat")
-const maxCharismaStat = document.getElementById("maxCharismaStat")
+const maxVitalityStat = document.getElementById("maxVitalityStat");
+const maxEnduranceStat = document.getElementById("maxEnduranceStat");
+const maxStrengthStat = document.getElementById("maxStrengthStat");
+const maxAgilityStat = document.getElementById("maxAgilityStat");
+const maxIntelligenceStat = document.getElementById("maxIntelligenceStat");
+const maxWisdomStat = document.getElementById("maxWisdomStat");
+const maxPerceptionStat = document.getElementById("maxPerceptionStat");
+const maxCharismaStat = document.getElementById("maxCharismaStat");
 //text
 const text = document.getElementById("text")
- 
+
+//clear
+function clear() {
+    text.innerText = "Loading . . ."
+    choices.innerHTML = ""
+}
+
 //Start
 function characterCreation() {
+    clear()
     text.innerText = "Build Your character"
     
-    const vitalityLabel = document.createElement('label')
-    vitalityLabel.for
-
-    const baseVitalityStat = document.createElement('input')
+    const vitalityLabel = document.createElement("label")
+    vitalityLabel.setAttribute("for", "baseVitalityStat")
+    vitalityLabel.innerText = "Vitality"
+    choices.appendChild(vitalityLabel)
+    const baseVitalityStat = document.createElement("input")
     baseVitalityStat.type = "number"
-    interface.appendChild(baseVitalityStat)
+    baseVitalityStat.min = 0
+    baseVitalityStat.id = "baseVitalityStat"
+    choices.appendChild(baseVitalityStat)
     
-    const baseEnduranceStat = document.createElement('input')
+    choices.appendChild(document.createElement("br"))
+    
+    const enduranceLabel = document.createElement("label")
+    enduranceLabel.setAttribute("for", "baseEnduranceStat")
+    enduranceLabel.innerText = "Endurance"
+    choices.appendChild(enduranceLabel)
+    const baseEnduranceStat = document.createElement("input")
     baseEnduranceStat.type = "number"
-    interface.appendChild(baseEnduranceStat)
+    baseEnduranceStat.min = 0
+    baseEnduranceStat.id = "baseEnduranceStat"
+    choices.appendChild(baseEnduranceStat)
 
-    const baseStrengthStat = document.createElement('input')
+    choices.appendChild(document.createElement("br"))
+    
+    const strengthLabel = document.createElement("label")
+    strengthLabel.setAttribute("for", "baseStrengthStat")
+    strengthLabel.innerText = "Strength"
+    choices.appendChild(strengthLabel)
+    const baseStrengthStat = document.createElement("input")
     baseStrengthStat.type = "number"
-    interface.appendChild(baseStrengthStat)
+    baseStrengthStat.min = 0
+    baseStrengthStat.id = "baseStrengthStat"
+    choices.appendChild(baseStrengthStat)
 
-    const baseAgilityStat = document.createElement('input')
+    choices.appendChild(document.createElement("br"))
+    
+    const agilityLabel = document.createElement("label")
+    agilityLabel.setAttribute("for", "baseAgilityStat")
+    agilityLabel.innerText = "Agility"
+    choices.appendChild(agilityLabel)
+    const baseAgilityStat = document.createElement("input")
     baseAgilityStat.type = "number"
-    interface.appendChild(baseAgilityStat)
+    baseAgilityStat.min = 0
+    baseAgilityStat.id = "baseAgilityStat"
+    choices.appendChild(baseAgilityStat)
 
-    const baseIntelligenceStat = document.createElement('input')
+    choices.appendChild(document.createElement("br"))
+    
+    const intelligenceLabel = document.createElement("label")
+    intelligenceLabel.setAttribute("for", "baseIntelligenceStat")
+    intelligenceLabel.innerText = "Intelligence"
+    choices.appendChild(intelligenceLabel)
+    const baseIntelligenceStat = document.createElement("input")
     baseIntelligenceStat.type = "number"
-    interface.appendChild(baseIntelligenceStat)
+    baseIntelligenceStat.min = 0
+    baseIntelligenceStat.id = "baseIntelligenceStat"
+    choices.appendChild(baseIntelligenceStat)
 
-    const baseWisdomStat = document.createElement('input')
+    choices.appendChild(document.createElement("br"))
+    
+    const wisdomStatLabel = document.createElement("label")
+    wisdomStatLabel.setAttribute("for", "baseWisdomStat")
+    wisdomStatLabel.innerText = "Wisdom"
+    choices.appendChild(wisdomStatLabel)
+    const baseWisdomStat = document.createElement("input")
     baseWisdomStat.type = "number"
-    interface.appendChild(baseWisdomStat)
+    baseWisdomStat.min = 0
+    baseWisdomStat.id = "baseWisdomStat"
+    choices.appendChild(baseWisdomStat)
 
-    const basePerceptionStat = document.createElement('input')
+    choices.appendChild(document.createElement("br"))
+    
+    const perceptionLabel = document.createElement("label")
+    perceptionLabel.setAttribute("for", "basePerceptionStat")
+    perceptionLabel.innerText = "Perception"
+    choices.appendChild(perceptionLabel)
+    const basePerceptionStat = document.createElement("input")
     basePerceptionStat.type = "number"
-    interface.appendChild(basePerceptionStat)
+    basePerceptionStat.min = 0
+    basePerceptionStat.id = "basePerceptionStat"
+    choices.appendChild(basePerceptionStat)
 
-    const baseCharismaStat = document.createElement('input')
+    choices.appendChild(document.createElement("br"))
+
+    const CharismaLabel = document.createElement("label")
+    CharismaLabel.setAttribute("for", "baseCharismaStat")
+    CharismaLabel.innerText = "Charisma"
+    choices.appendChild(CharismaLabel)
+    const baseCharismaStat = document.createElement("input")
     baseCharismaStat.type = "number"
-    interface.appendChild(baseCharismaStat)
+    baseCharismaStat.min = 0
+    baseCharismaStat.id = "baseCharismaStat"
+    choices.appendChild(baseCharismaStat)
+
+    choices.appendChild(document.createElement("br"))
+
+    const resetButton = document.createElement("button")
+    resetButton.id = "resetButton"
+    resetButton.innerText = "Reset"
+    resetButton.addEventListener("click", () => {
+        characterCreation()
+    })
+    choices.appendChild(resetButton)
+    const doneButton = document.createElement("button")
+    doneButton.id = "doneButton"
+    doneButton.innerText = "Done"
+    doneButton.addEventListener("click", () => {
+        if (baseVitalityStat.value + 
+            baseEnduranceStat.value + 
+            baseStrengthStat.value + 
+            baseAgilityStat.value + 
+            baseIntelligenceStat.value + 
+            baseWisdomStat.value +
+            basePerceptionStat.value +
+            baseCharismaStat.value ==
+            5
+        ) {
+            alert("stuff")
+        }
+        else {
+            alert("invaild")
+        }
+    })
+    choices.appendChild(doneButton)
 }
 
 function stuff() {
-    const newButton = document.createElement('button');
-    newButton.textContent = 'Click me!';
+    const newButton = document.createElement("button");
+    newButton.textContent = "Click me!";
     interface.appendChild(newButton);
-    newButton.addEventListener('click', () => {
-        alert('New button clicked!');
+    newButton.addEventListener("click", () => {
+        alert("New button clicked!");
       });
 }
+
+
