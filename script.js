13986761ead301e2eab49de5705913e1896fb842
Benny <158503969+TrollFacedGamer@users.noreply.github.com>
2024-08-22 23:07:18 -0400

    0.001.002 Hunger/Thirst/$/INV/stage1
    added hunger, thirst, money and inventory base and started on stage 1

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const interface = document.getElementById("interface");
     const choices = document.getElementById("choices");
 const credits = document.getElementById("credits")
 //ElementStat
+const money = document.getElementById("money")
+
 const levelStat = document.getElementById("levelStat")
 const currentEXPStat = document.getElementById("currentEXPStat")
 const maxEXPStat = document.getElementById("maxEXPStat")
@@ -53,6 +55,8 @@ function resetStat() {
 }
 //text
 const text = document.getElementById("text")
+const sceneNumber = document.getElementById("sceneNumber")
+const character = document.getElementById("character")
 
 //clear
 function clear() {
@@ -64,6 +68,7 @@ function clear() {
 function characterCreation() {
     clear()
     text.innerText = "Build Your character"
+    sceneNumber.innerText = "000"
     
     const vitalityLabel = document.createElement("label")
     vitalityLabel.setAttribute("for", "baseVitalityStat")
@@ -230,7 +235,8 @@ function characterCreation() {
         resetStat()
 
         if (roleSelection.innerText = "Priest") {
-            priestScene1Day1()
+            priestScene001()
+            character.innerText = "priest"
         }
         else {
             characterCreation()
@@ -243,11 +249,148 @@ function characterCreation() {
     }
 }
 
-function priestScene1Day1() {
-    clear()
-    
+function priestScene001() {
+    clear();
+    text.innerText = "You wake up to the sound of your alarm tucked in your bed, before slaping your alarm clock off.";
+    sceneNumber.innerText = "001";
+
+    const getUp = document.createElement("button");
+    getUp.innerText = "Get up";
+    getUp.addEventListener("click", () => {
+        priestScene002();
+    });
+    choices.appendChild(getUp);
 }
 
+function priestScene002() {
+    clear();
+    text.innerText = "You look groggily around your old house.";
+    sceneNumber.innerText = "002";
+    
+    const bed = document.createElement("button");
+    bed.innerText = "Bed";
+    bed.addEventListener("click",() => {
+        priestScene004();
+    });
+    choices.appendChild(bed);
+
+    const poseter = document.createElement("button");
+    poseter.innerText = "Poseter";
+    poseter.addEventListener("click", () => {
+        priestScene006();
+    })
+    choices.appendChild(poseter);
+
+    const stove = document.createElement("button");
+    stove.innerText = "Stove"
+    stove.addEventListener("click", () => {
+        priestScene007()
+    });
+    choices.appendChild(stove)
+}
+
+function priestScene004() {
+    clear();
+    text.innerText = "You feel the exhaustion of your labored flesh and its yearning for more rest.";
+    sceneNumber.innerText = "004";
+
+    const nextScene = document.createElement("button");
+    nextScene.innerText = "continue";
+    nextScene.addEventListener("click", () => {
+        priestScene011();
+    });
+    choices.appendChild(nextScene);
+};
+
+function priestScene005() {
+    clear();
+    text.innerText = "You resist the temptation from your bed.";
+    sceneNumber.innerText = "005";
+
+    const nextScene = document.createElement("button");
+    nextScene.innerText = "continue";
+    nextScene.addEventListener("click", () => {
+        priestScene002();
+    });
+    choices.appendChild(nextScene);
+};
+
+function priestScene006() {
+    clear();
+    text.innerText = "Kelp. It is sustenance. It is fuel. It is god.";
+    sceneNumber.innerText = "006";
+
+    const back = document.createElement("button");
+    back.innerText = "Back";
+    back.addEventListener("click", () => {
+        priestScene002();
+    });
+    choices.appendChild(back)
+};
+
+function priestScene007() {
+    clear();
+    text.innerText = "You stare at your empty pot and feel your stomach rumble in hunger.";
+    sceneNumber.innerText = "007";
+
+    if () {
+        
+    }
+};
+
+function priestScene008() {
+    clear();
+    text.innerText = "";
+    sceneNumber.innerText = "";
+
+    
+};
+
+function priestScene009() {
+    clear();
+    text.innerText = "";
+    sceneNumber.innerText = "";
+
+    
+};
+
+function priestScene010() {
+    clear();
+    text.innerText = "";
+    sceneNumber.innerText = "";
+
+    
+};
+
+function priestScene011() {
+    clear()
+    text.innerText = "Do you return to sleep?"
+    sceneNumber.innerText = "011"
+
+    const yes = document.createElement("button")
+    yes.innerText = "Yes"
+    yes.addEventListener("click", () => {
+        priestScene031()
+    })
+    choices.appendChild(yes)
+    
+    const no = document.createElement("button")
+    no.innerText = "No"
+    no.addEventListener("click", () => {
+        priestScene005()
+    })
+    choices.appendChild(no)
+}
+
+function priestScene031() {
+    clear()
+    text.innerText = "You give in to your exhaustion and fall onto the bed."
+    sceneNumber.innerText = "031"
+    
+    const toBeContinued = document.createElement("button")
+    toBeContinued.innerText = "Coming Soon"
+    choices.appendChild(toBeContinued)
+}
 /*
 function stuff() {
     const newButton = document.createElement("button");
